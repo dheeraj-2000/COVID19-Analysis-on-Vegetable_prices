4d02bb06afde41f6c255764e70215ecbbd064988
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-30 15:14:20 +0530

    updated covid analysis

---
+++ Onion_Analysis.R
@@ -0,0 +1,4 @@
+
+data1 <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/Onion/Onion_2020_correction2_weeklyAvg.csv")
+data1
+"colnames(data1) <- c()
