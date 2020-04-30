353ab46079a6d2f637dc0f4148023163491f819e
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-30 19:22:27 +0530

    updated covid analysis

--- Onion_Analysis.R
+++ Onion_Analysis.R
@@ -1,6 +1,9 @@
 library(oddsratio)
 library(dplyr)
 library(ggplot2)
+library(Metrics)
+library(forecast)
+library(reshape)
 data1 <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/Onion/Onion_2020_correction2_weeklyAvg.csv")
 data1
 colnames(data1) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
@@ -12,6 +15,9 @@ View(summary(data1))
 ############################
 onion_state <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/state/Onion_2020_correction3_weeklyAvg.csv")
 summary(data1[2:16])
+boxplot(onion_state$Chattisgarh)
+outvalues = boxplot(onion_state$Chattisgarh)$out
+outvalues
 onion_country <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/country/Onion_2020_correction2_weeklyAvg.csv")
 colnames(data1) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
 summary(data1[2:9])
@@ -25,7 +31,13 @@ summary(potato_country[2:9])
 
 
 tomato_state <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/state/Tomato_2020_correction3_weeklyAvg.csv")
+tomato_state
+start(tomato_state)
 summary(tomato_state[2:16])
+boxplot(tomato_state$Chattisgarh)
+outvalues = boxplot(tomato_state$Chattisgarh)$out
+outvalues
+my_Object <- ts(tomato_state, start="w1" ,frequency = 15)
 tomato_country <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/country/Tomato_2020_correction2_weeklyAvg.csv")
 colnames(tomato_country) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
 summary(tomato_country[2:9])
