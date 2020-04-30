2b9725751731d792f0a4a69685334e320afe251f
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-30 18:04:12 +0530

    updated covid analysis

--- Onion_Analysis.R
+++ Onion_Analysis.R
@@ -1,6 +1,17 @@
-
+library(oddsratio)
+library(dplyr)
+library(ggplot2)
 data1 <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/Onion/Onion_2020_correction2_weeklyAvg.csv")
 data1
 colnames(data1) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
 data1
-library(oddsratio)
+glimpse(data1)
+sapply(data1, mean, na.rm=TRUE)
+View(summary(data1))
+
+onion_state <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/state/Onion_2020_correction3_weeklyAvg.csv")
+summary(data1[2:16])
+onion_country <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/country/Onion_2020_correction2_weeklyAvg.csv")
+colnames(data1) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
+summary(data1[2:9])
+
