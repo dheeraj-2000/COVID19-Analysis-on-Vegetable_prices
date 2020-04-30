35746973168c0dae4fb74a0586795fb1c3d4422f
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-30 19:52:16 +0530

    updated covid analysis

--- Onion_Analysis.R
+++ Onion_Analysis.R
@@ -24,9 +24,20 @@ summary(data1[2:9])
 Potato_2020_correction2_weeklyAvg.csv
 
 potato_state <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/state/Potato_2020_correction3_weeklyAvg.csv")
+potato_state.ts <- as.ts(potato_state)
+my_Object <- ts(potato_state, start=1,frequency = 15)
+boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of tomato in the country considering 15 states")
+outvalues = boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of tomato in the country considering 15 states")$out
+outvalues
 summary(potato_state[2:16])
+
 potato_country <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/country/Potato_2020_correction2_weeklyAvg.csv")
 colnames(potato_country) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
+potato_country.ts <- as.ts(potato_country)
+my_Object <- ts(potato_country, start=1,frequency = 16)
+boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of tomato in the country considering 15 states")
+outvalues = boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of potato in the country considering 15 states")$out
+outvalues
 summary(potato_country[2:9])
 
 
@@ -35,7 +46,6 @@ tomato_state
 tomato_state.ts <- as.ts(tomato_state)
 my_Object <- ts(tomato_state, start=1,frequency = 15)
 boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of tomato in the country considering 15 states")
-boxplot(tomato_state$Chattisgarh)
 outvalues = boxplot(my_Object~cycle(my_Object),xlab="weeks",ylab = "states_price",main = "Prices of tomato in the country considering 15 states")$out
 outvalues
 summary(tomato_state[2:16])
