f1f5bcd14b0f471e360f30109229f3a708204514
dheeraj2000 <dheeraj12000@gmail.com>
2020-04-30 15:28:49 +0530

    updated covid analysis

--- Onion_Analysis.R
+++ Onion_Analysis.R
@@ -1,4 +1,6 @@
 
 data1 <- read.csv("/home/dheeraj/my_projects/my_project_env/practice/COVID19-Analysis-on-Vegetable_prices/Onion/Onion_2020_correction2_weeklyAvg.csv")
 data1
-"colnames(data1) <- c()
+colnames(data1) <- c("states", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8")
+data1
+library(oddsratio)
